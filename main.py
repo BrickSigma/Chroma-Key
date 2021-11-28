96361658425d10f77d4c4eb97abd516cba3c3456
Junaid Chaudhry <jchaudhry05@gmail.com>
2021-11-28 12:18:52 +0300

    Minor adjustments.
    Edited comments and removed timer from program.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import cv2
 import numpy as np
-from numba import jit, int32
-import time
+from numba import jit
 
 CUDA_AVAILABLE = False
 try:
-    #raise ImportError
     from numba import cuda
     if cuda.is_available():
         cuda.detect()
@@ -17,7 +15,7 @@ except ImportError:
 
 from color_range import Analysis  # This is a class built for analysing the background
 
-img = cv2.imread("path.png")  # Load in the background image path of the video here
+img = cv2.imread("bg.png")  # Load in the background image path of the video here
 HEIGHT, WIDTH = img.shape[:2]
 print(f"path.png: WIDTH={WIDTH}, HEIGHT={HEIGHT}")
 
@@ -25,8 +23,8 @@ all_rgb_codes = img.reshape(-1, img.shape[-1])
 colors = np.unique(all_rgb_codes, axis=0)
 print(f"path.png: {colors.shape[0]} unique colours")
 
-#colors = list(colors)  # Convert the set to a list so that it can be used by the class
 analize = Analysis(colors)  # This creates the analysis object. It holds all the details and function for removing the background
+analize.calc_offset()
 analize.range_format()  # This will create a specialised range of equations used to remove the background (more info in color_range.py file)
 equations_xy, equations_zy = analize.get_rules()
 print(f"equations_xy: {equations_xy}")
@@ -36,24 +34,24 @@ if CUDA_AVAILABLE:
     equations_xy = cuda.to_device(equations_xy)
     equations_zy = cuda.to_device(equations_zy)
 
-vidcap = cv2.VideoCapture('video.mp4')  # Load in the video you want edited
+vidcap = cv2.VideoCapture("video.mp4")  # Load in the video you want edited
 video_name = 'test.mp4'  # This is the new edited video's name
 fps = vidcap.get(cv2.CAP_PROP_FPS)
 success,image = vidcap.read()  # Reads the first frame
 height, width, layers = image.shape
 print(f"video.mp4: Width={width}, Height={height}")
 
-video = cv2.VideoWriter(video_name, 0, fps, (width,height))  # Set up the video writer 
+video = cv2.VideoWriter(video_name, 0, 30, (width,height))  # Set up the video writer 
 
 @jit(nopython=True)
 def check_colors(analize,image):
-    # Loop through the frame's pixels, this part is slow.
     for h in range(height):
         for w in range(width):
             if analize.check_color(image[h, w]):  # This calls one of the functions in the Analize class that chacks whether the color is in in the set range or not. This is one of the slow parts
+                # Change the color to red
                 image[h, w, 0] = 0
                 image[h, w, 1] = 0
-                image[h, w, 2] = 255  # Change the color to red
+                image[h, w, 2] = 255
 
 
 if CUDA_AVAILABLE:
@@ -93,22 +91,17 @@ if CUDA_AVAILABLE:
             d_Result.copy_to_host(ary=A,stream=stream) # copy from d_Result (on device) back to A (on host)
 
 # Main loop that goes through every frame in the video and edits them
-count = 0
 while success:
     
-    t1 = time.time()
     if CUDA_AVAILABLE:
         cuda_check_colors(image, equations_xy, equations_zy)
     else:
         check_colors(analize, image)
-    t2 = time.time()
-    print(f'check_colors took {t2-t1}s')
 
     video.write(image)  # Add the new frame to the video
 
     success,image = vidcap.read()  # Read the next frame
-    print('Read a new frame: ', success)
-    count += 1
 
-cv2.destroyAllWindows()
+vidcap.release()
 video.release()  # Release the video
+cv2.destroyAllWindows()
